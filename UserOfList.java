3c908a04e5b0cf91410391a815a8615d7452ff1f
Tanvir Ahmed <tahmed5@stuy.edu>
2019-03-06 22:11:38 -0500

    small insignificant edits

--- UserOfList.java
+++ UserOfList.java
@@ -8,9 +8,9 @@ public class UserOfList {
         // System.out.println( "number of elements: " + list.size() );
         System.out.println( "empty list:" + list);
 
-        // /* Populate the list with elements, but with a small enough
-           // number that we expect no invocation of expand().
-        // */
+        /* Populate the list with elements, but with a small enough
+           number that we expect no invocation of expand().
+        */
         int i = 0;
         for( ; i < 5; i++ ) {
             list.add( -i); // differs from index, but similar
